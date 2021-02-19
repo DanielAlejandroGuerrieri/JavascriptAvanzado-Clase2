bafeda9a8df475044c8be2484acb6662b7a37d2b
DanielAlejandroGuerrieri <daniel.guerrieri@gmail.com>
2021-02-19 17:11:22 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import capitalizar from "./capitalizar.js";
+const _ = require('lodash');
+
+
+console.log(capitalizar("hola mundo"));
+
+console.log(_.capitalize("hola mundo, desde lodash"));
+
+console.log(_.capitalize("emi no sabe mas nada"));
